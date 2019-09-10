d1ad1a41f9a507930447ecdd843cd9c2390fd054
Administrator <Administrator@DESKTOP-656J2TR>
2019-09-10 16:56:54 +0800

    作业代码提交

--- src/pro01/TestZuoye.java
+++ src/pro01/TestZuoye.java
@@ -1,4 +1,24 @@
+/**
+ * 1.Java是一种强类型语言，说明Java的数据类型分类。
+ * 整型：byte short int long 
+ * 浮点：float double
+ * 字符：char
+ * 布尔：Boolean
+ * 引用类型：class interface 数组
+ 
+2.i++和++i的异同之处
+i++是先使用值，++i是先自增再使用
+
+3.运算符||和|的异同之处
+短路可以提升代码性能 
+
+4.Java中基本数据类型转换的规则
+
+ */
+
 package pro01;
+
+import java.util.Scanner;
 
 public class TestZuoye {
 	public static void main(String[] args) {
@@ -14,16 +34,27 @@ public class TestZuoye {
 //		System.out.println("两年活期后本金和利息="+Math.round(c*(Math.pow((1+0.0035), 2))));
 //		System.out.println("一年定期后本金和利息="+Math.round(c*(1+0.015)));
 //		System.out.println("两年定期后本金和利息="+Math.round(c*(Math.pow((1+0.021), 2))));
-		int a = 1234;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("输入4位数字：");
+		int a = scanner.nextInt();
 		int a1 = a/1000;
 		int a2 = (a-a1*1000)/100;
 		int a3 = (a-a1*1000-a2*100)/10;
 		int a4 = a-a1*1000-a2*100-a3*10;
-		System.out.printf("%d,%d,%d,%d", a1,a2,a3,a4);
+//		System.out.printf("%d,%d,%d,%d", a1,a2,a3,a4);
 		
 		int t1=0,t2=0;
-		a1 = t1;
+		t1 = a1;
+		a1 = a4;
+		a4 = t1;
 		
+		t2 = a2;
+		a2 = a3;
+		a3 = t2;
+		System.out.printf("%d,%d,%d,%d \n",a1,a2,a3,a4 );
+		
+		System.out.println(a1*1000+a2*100+a3*10+a4);
 		
 	}
 }

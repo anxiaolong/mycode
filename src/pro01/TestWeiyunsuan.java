1f71dcba2ef9fc6f93790d0c164b9eaa2014135c
Administrator <Administrator@DESKTOP-656J2TR>
2019-09-10 14:53:50 +0800

    数据类型和运算符 测试完成

--- src/pro01/TestWeiyunsuan.java
+++ src/pro01/TestWeiyunsuan.java
@@ -11,11 +11,15 @@ public class TestWeiyunsuan {
 //		System.out.println(~15); // 
 		
 		//三目运算测试
-		int a = 10;
-		int b = -3;
-		String s = a>0 ?"test":"pass";
-		System.out.println(s);
-		int x = a<0 ? 3 : (b>0?5:8);
-		System.out.println(x);
+//		int a = 10;
+//		int b = -3;
+//		String s = a>0 ?"test":"pass";
+//		System.out.println(s);
+//		int x = a<0 ? 3 : (b>0?5:8);
+//		System.out.println(x);
+		short a = 11;
+//		short b = 2222222222; 超过表数范围
+		
+
 	}
 }

4eead45f9bf6d722003bb56d2df6731b57441c5a
Administrator <Administrator@DESKTOP-656J2TR>
2019-09-17 17:51:07 +0800

    test abstract

---
+++ src/oop/TestAbstract.java
@@ -0,0 +1,26 @@
+package oop;
+/**
+ * abstract修饰的类，不能new
+ * abstract修饰的方法，必须在子类重写
+ * abstract方法的类，必须用abstract修饰
+ * @author Administrator
+ *
+ */
+public class TestAbstract {
+	public static void main(String[] args) {
+//		TestAbstract1 testAbstract1 = new TestAbstract1(); //abstract修饰的类不能new对象
+		TestAbstract2 t1 = new TestAbstract2();
+		t1.run();
+	}
+}
+
+abstract class TestAbstract1 { //有abstract方法的类，必须用abstract修饰
+	abstract void run(); //abstract 修饰的方法没有实现的语句块
+}
+
+class TestAbstract2 extends TestAbstract1 {
+	//继承父类abstract方法，必须要重写
+	public void run() { 
+		System.out.println("跑");
+	}
+}

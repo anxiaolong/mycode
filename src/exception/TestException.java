f5a8f6ad8400c68cefa926de2bb8aca1a8369203
Administrator <Administrator@DESKTOP-656J2TR>
2019-09-23 14:33:32 +0800

    测试自定义异常类

---
+++ src/exception/TestException.java
@@ -0,0 +1,42 @@
+package exception;
+/**
+ * 自定义异常类测试
+ * @author Administrator
+ *
+ */
+public class TestException {
+	public static void main(String[] args) {
+		TestException1 t1Exception1 = new TestException1();
+		try {
+			t1Exception1.setAge(-1);
+		} catch (MyException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+//自定义异常类
+class MyException extends RuntimeException {
+	public MyException() {}
+	//自定义异常类对象的构造方法
+	public MyException(String s) {
+		super(s); //输出字符串s
+	}
+}
+
+class TestException1 {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age<0) {
+			throw new MyException("年龄不能为负数");
+		}
+		this.age = age;
+	}
+	
+}
+

d0da2f99277d9ff0899bd6bed1ac923389543356
Administrator <Administrator@DESKTOP-656J2TR>
2019-10-01 11:40:52 +0800

    update

--- src/cy_class/TestCalendar.java
+++ src/cy_class/TestCalendar.java
@@ -1,55 +1,62 @@
 package cy_class;
-
-import java.sql.Date;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
-
 /**
  * 可视化日历
  * @author Administrator
  *
  */
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+
 public class TestCalendar {
 	public static void test1() {
-		//从键盘获取yyyy-MM-dd格式的日期
-		System.out.println("请输入日期，格式如：2019-01-01:\n");
-		Scanner s = new Scanner(System.in);
-		String s_date = s.next();
 		
-		//将输入的字符串转化为日历类对象
-		String[] s1 = s_date.split("-"); //以-将字符串床划分为数组
-		int year = Integer.parseInt(s1[0]); //直接将字符串转换为整型数
-		int month = Integer.parseInt(s1[1]);
-		int dayOfMonth = Integer.parseInt(s1[2]);
-		GregorianCalendar g = new GregorianCalendar(year, month-1, dayOfMonth); //月是0-11
-		
-		g.set(Calendar.DAY_OF_MONTH, 1); //设置日期为当月1号
-		int dow = g.get(Calendar.DAY_OF_WEEK); //1号对应的日期是一周的第几天
-//		System.out.println(dow);  //1号对应的是一周的第三天
-		System.out.println("日\t一\t二\t三\t四\t五\t六");
-		for (int i = 0; i < dow - 1; i++) {
-            System.out.print("\t");  //打印两个空格，1号被放在星期二位置
-        }
-		int maxDate = g.getActualMaximum(Calendar.DAY_OF_MONTH); //获取当月日期最大值
-//        System.out.println("maxDate:"+maxDate);
-        for (int i = 1; i <= maxDate; i++) {  //把1-31号的日期全部打印出来
-            StringBuilder sBuilder = new StringBuilder();
-            if (g.get(Calendar.DATE) == dayOfMonth) {
-                sBuilder.append(g.get(Calendar.DATE) + "*\t"); //如何和输入日期相同加*
-            } else {
-                sBuilder.append(g.get(Calendar.DATE) + "\t");
-            }
-            System.out.print(sBuilder);
- 
-            if (g.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) { 
-                System.out.print("\n"); //遇到星期六 换行
-            }
-            g.add(Calendar.DATE, 1);//循环一次，日期数加1
-        }
 	}
 	public static void main(String[] args) {
-			test1();
+		//键盘输入
+		System.out.println("输入格式如yyyy-MM-dd的日期：");
+		Scanner inScanner = new Scanner(System.in);
+		String aString = inScanner.next();
+		
+		String[] a = aString.split("-"); //将字符串拆分为数组
+		System.out.println(Arrays.toString(a)); 
+		int y = Integer.parseInt(a[0]);
+		int m = Integer.parseInt(a[1]);
+		int d = Integer.parseInt(a[2]);
+		//new一个GregorianCalendar对象
+		GregorianCalendar g = new GregorianCalendar(y,m-1,d); //月份一定要-1
+		System.out.println("日\t一\t二\t三\t四\t五\t六");
+		g.set(Calendar.DAY_OF_MONTH, 1); //g的日期设置为1号
+//		System.out.println(g.get(Calendar.DAY_OF_WEEK)); //当前日期是一周中的第几天
+		for (int i = 1; i < g.get(Calendar.DAY_OF_WEEK); i++) {
+			System.out.print("\t"); //日历第一行空格，根据是一周的第几天计算
+		}
+		int max = g.getActualMaximum(Calendar.DAY_OF_MONTH); //获取当月最大天数
+		for (int i = 1; i <= max; i++) {
+			if ((g.get(Calendar.DAY_OF_MONTH)==d)) {
+				System.out.print(g.get(Calendar.DAY_OF_MONTH)+"*\t"); //判断如果是当日加*
+			} else {
+				System.out.print(g.get(Calendar.DAY_OF_MONTH)+"\t");
+			}	
+			if (g.get(Calendar.DAY_OF_WEEK)==7) {
+				System.out.println(); //判断是星期六，换行
+			}
+			g.add(Calendar.DAY_OF_MONTH, 1);
+		}
 		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+

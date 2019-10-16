34c88a8093ca186b662e80797a30bf8ba1f96a91
Administrator <Administrator@DESKTOP-656J2TR>
2019-10-16 14:30:36 +0800

    测试IOUtils

---
+++ src/io/TestIOUtils.java
@@ -0,0 +1,16 @@
+package io;
+/**
+ * 测试IOUtils类，主要针对流进行操作
+ * FileUtils类，主要针对文件进行操作
+ */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+
+public class TestIOUtils {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String s1 = IOUtils.toString(new FileInputStream("D:\\eclipse-workspace\\mycode\\src\\io\\test.txt"), "gbk");
+		System.out.println(s1);
+	}
+}

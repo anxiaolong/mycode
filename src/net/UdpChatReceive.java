d5d467c59af0630a85a22cf8680d0aff4ac9cad5
Administrator <Administrator@DESKTOP-656J2TR>
2019-10-28 16:29:22 +0800

    使用UDP实现学生和老师对聊

---
+++ src/net/UdpChatReceive.java
@@ -0,0 +1,34 @@
+package net;
+/**
+ * 使用UDP实现学生和老师对聊
+ */
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UdpChatReceive extends Thread {
+	private DatagramSocket receiveSocket;
+	
+	public UdpChatReceive(int port) throws SocketException {
+		receiveSocket = new DatagramSocket(port);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				byte[] packet = new byte[1024];
+				DatagramPacket receivePacket = new DatagramPacket(packet, packet.length);
+				receiveSocket.receive(receivePacket);
+				byte[] packet1 = receivePacket.getData();
+				String s = new String(packet1);
+				System.out.println(s);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		receiveSocket.close();
+	}
+}
---
+++ src/net/UdpChatSend.java
@@ -0,0 +1,44 @@
+package net;
+/**
+ * 使用UDP实现学生和老师对聊
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UdpChatSend extends Thread  {
+	private InetAddress toip;
+	private int toipPort;
+	private DatagramSocket sendSocket;
+	
+	public UdpChatSend(int port,InetAddress toip,int toipPort) throws SocketException {
+		this.toip = toip;
+		this.toipPort = toipPort;
+		this.sendSocket = new DatagramSocket(port);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String s = br.readLine();
+				if (s.equals("end")) {
+					break;
+				}
+				byte[] packet = s.getBytes();
+				DatagramPacket senPacket = new DatagramPacket(packet, 0, packet.length, toip, toipPort);
+				sendSocket.send(senPacket);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			sendSocket.close();
+		}
+	}
+}

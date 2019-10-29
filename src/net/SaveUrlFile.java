49eaf4688c724e48d11e60385d7a844cb53269d9
Administrator <Administrator@DESKTOP-656J2TR>
2019-10-29 16:02:05 +0800

    将一个URL资源保存到本地

---
+++ src/net/SaveUrlFile.java
@@ -0,0 +1,22 @@
+package net;
+/**
+ * 将一个URL资源保存到本地
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class SaveUrlFile {
+	public static void main(String[] args) throws IOException {
+		URL u1 = new URL("http://cdnmusic.migu.cn/picture/2019/1029/0928/AM86cbbc02dcf74975a155a57bb592b7fc.jpg");
+		InputStream is = u1.openStream();
+		FileOutputStream fos = new FileOutputStream(new File("D:\\eclipse-workspace\\mycode\\src\\net\\URL.jpg"));
+		int temp = 0;
+		while ((temp=is.read())!=-1) {
+			fos.write(temp);
+		}
+		fos.close();
+	}
+}

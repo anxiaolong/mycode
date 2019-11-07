57caa084784630598e6ad35c3993a67bb97b0846
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-07 16:39:09 +0800

    封装了请求，并将参数转为map

--- src/httpserver/D3Server02.java
+++ src/httpserver/D3Server02.java
@@ -1,9 +1,9 @@
 package httpserver;
 /**
  * 封装响应response
+ * 封装请求request
  */
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -33,18 +33,22 @@ private ServerSocket serverSocket;
 			System.out.println("一个连接进来");
 			
 			//获取请求协议
-			InputStream is = socket.getInputStream();
-			byte[] datas = new byte[1024*1024];
-			int len = is.read(datas);
-			String requestInfo = new String(datas, 0, len);
-			System.out.println(requestInfo);
-			
+			D3Request request = new D3Request(socket);
+			request.parseRequestInfo();
+			request.coverMap();
 			//响应信息
 			D3Response response = new D3Response(socket);
-			response.print("这是server返回的结果");
+			response.print("<html>");
+			response.print("<head>");
+			response.print("<title>");
+			response.print("这是server返回的结果：");
+			response.print("</title>");
+			response.print("</head>");
+			response.print("<body>");
+			response.print("响应成功了");
+			response.print("</body>");
+			response.print("</html>");
 			response.pushToBrowser(200);
-			System.out.println(response.headInfo);
-			System.out.println(response.content);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

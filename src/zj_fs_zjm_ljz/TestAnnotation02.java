e9d8d93d3cfb5621708ea51da995e264dc2dae72
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-11 13:59:06 +0800

    测试自定义注解的使用

---
+++ src/zj_fs_zjm_ljz/TestAnnotation02.java
@@ -0,0 +1,19 @@
+package zj_fs_zjm_ljz;
+/**
+ * 测试自定义注解的使用
+ * @author Administrator
+ *
+ */
+@Annotation01
+public class TestAnnotation02 {
+	@Annotation01(studentName="anxiaolong",age=27,id=1,
+			school= {"成都大学"})
+	public void test1() {
+		
+	}
+	
+	@Annotation02(value = "test")
+	public void test2() {
+		
+	}
+}

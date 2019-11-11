1dfb46a3967a6abbf8a671e235a40422d01c7f27
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-11 10:23:10 +0800

    测试常用三个注解Override Deprecated SuppressWarnning

---
+++ src/zj_fs_zjm_ljz/TestAnnotation01.java
@@ -0,0 +1,29 @@
+package zj_fs_zjm_ljz;
+/**
+ * 测试常用三个注解Override Deprecated SuppressWarnning
+ * @author Administrator
+ *
+ */
+public class TestAnnotation01 {
+	
+	@Override //override注解 重写父类的方法
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	@Deprecated //deprecated表示不建议使用的方法，强用也行
+	public static void print() {
+		System.out.println("test");
+	}
+	
+	@SuppressWarnings(value = { "all" }) //抑制编译警告
+	public static void getInput() {
+		int m;
+		String string;
+	}
+	
+	public static void main(String[] args) {
+		TestAnnotation01.print();
+	}
+}

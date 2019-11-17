f0c768de77ef9e44ea6b4737c076564e925579af
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-17 15:03:41 +0800

    测试javassist操作类、属性、方法

---
+++ src/zj_fs_zjm_ljz/Javassist_Emp.java
@@ -0,0 +1,31 @@
+package zj_fs_zjm_ljz;
+
+import java.io.Serializable;
+
+@SuppressWarnings("all")
+public class Javassist_Emp implements Serializable {
+	private int id;
+	private String name;
+	
+	public Javassist_Emp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+		System.out.println(id);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

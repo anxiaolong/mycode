285e1f9931f6a6e31d6636613352ff26ccf2a012
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-20 14:11:56 +0800

    测试线程上下文类加载器

---
+++ src/zj_fs_zjm_ljz/Ljz_test05.java
@@ -0,0 +1,24 @@
+package zj_fs_zjm_ljz;
+/**
+ * 测试线程上下文类加载器
+ * @author Administrator
+ *
+ */
+public class Ljz_test05 {
+	public static void main(String[] args) throws Exception {
+		//应用类加载器
+		ClassLoader loader1 = Ljz_test05.class.getClassLoader();
+		System.out.println(loader1);
+		
+		ClassLoader loader2 = Thread.currentThread().getContextClassLoader();
+		System.out.println(loader2);
+		
+		//为当前线程设置新的加载器，不适用默认加载器
+		Thread.currentThread().setContextClassLoader(new Ljz_FileSystemClassLoader("c:/test"));
+		System.out.println(Thread.currentThread().getContextClassLoader());
+		
+		Class<Ljz_test05> c = (Class<Ljz_test05>)Thread.currentThread().getContextClassLoader().loadClass("zj_fs_zjm_ljz.Ljz_test05");
+		System.out.println(c);
+		System.out.println(c.getClassLoader());
+	}
+}

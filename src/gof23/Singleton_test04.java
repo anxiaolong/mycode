f8711bdffa33f0a21160cb3bbb98a0387257ca02
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-21 15:55:14 +0800

    测试使用静态内部类实现单例模式，调用效率高，并且实现了懒加载

---
+++ src/gof23/Singleton_test04.java
@@ -0,0 +1,19 @@
+package gof23;
+/**
+ * 测试使用静态内部类实现单例模式
+ * 优点：线程安全、调用效率高、实现了懒加载
+ * @author Administrator
+ *
+ */
+public class Singleton_test04 {
+	private static class SingletonClassInstance {
+		private static final Singleton_test04 instance = new Singleton_test04();
+	}
+	
+	private Singleton_test04() {}
+	
+	//方法没有线程同步，调用效率高
+	public Singleton_test04 getInstance() {
+		return SingletonClassInstance.instance;
+	}
+}

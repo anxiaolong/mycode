ab2cca2e69ba0a162f5323fc1d576b025d94cf35
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-27 11:23:36 +0800

    测试适配器模式

---
+++ src/gof23/Adapter_Adapter2.java
@@ -0,0 +1,23 @@
+package gof23;
+/**
+ * 使用组合模式，实现适配器
+ * @author Administrator
+ *
+ */
+public class Adapter_Adapter2 implements Adapter_Target {
+	
+	private Adapter_Keyboard keyboard;
+	
+	public Adapter_Adapter2(Adapter_Keyboard keyboard) {
+		this.keyboard = keyboard;
+	}
+	
+	public void name() {
+		
+	}
+
+	@Override
+	public void handleInput() {
+		keyboard.acceptInput();
+	}
+}

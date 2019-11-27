2254e3014646622e1fda26fc0cda6dc5c517c69f
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-27 16:13:18 +0800

    测试静态代理模式

---
+++ src/gof23/ProxyStatic_star.java
@@ -0,0 +1,13 @@
+package gof23;
+/**
+ * 定义star接口
+ * @author Administrator
+ *
+ */
+public interface ProxyStatic_star {
+	public void signContract();
+	
+	public void sing();
+	
+	public void collectMoney();
+}

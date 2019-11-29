9f13f00ac5739068a617d4156fec0cbf033ad26d
Administrator <Administrator@DESKTOP-656J2TR>
2019-11-29 17:40:34 +0800

    测试装饰器模式

---
+++ src/gof23/Decorator_Icar.java
@@ -0,0 +1,91 @@
+package gof23;
+
+//抽象构建
+public interface Decorator_Icar {
+	void move();
+}
+
+
+//具体构建角色
+class Car implements Decorator_Icar {
+
+	@Override
+	public void move() {
+		System.out.println("陆地上跑");
+	}
+	
+}
+
+//装饰角色
+class SuperCar implements Decorator_Icar{
+	protected Decorator_Icar car;
+	
+	public SuperCar(Decorator_Icar car) {
+		this.car = car;
+	}
+	
+	@Override
+	public void move() {
+		car.move();
+	}
+}
+
+//具体装饰角色
+class FlyCar extends SuperCar{
+	public FlyCar(Decorator_Icar car) {
+		super(car);
+	}
+	
+	public void fly() {
+		System.out.println("飞上天");
+	}
+	
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		super.move();
+		fly();
+	}
+}
+
+class WaterCar extends SuperCar {
+	public WaterCar(Decorator_Icar car) {
+		super(car);
+	}
+	
+	public void swim() {
+		System.out.println("水上游");
+	}
+	
+	@Override
+	public void move() {
+		super.move();
+		swim();
+	}
+}
+
+class AICar extends SuperCar{
+	public AICar(Decorator_Icar car) {
+		super(car);
+	}
+	
+	public void autoMove() {
+		System.out.println("自动跑");
+	}
+	
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		super.move();
+		autoMove();
+	}
+}
+
+
+
+
+
+
+
+
+

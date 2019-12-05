298eb52db435151bbfa09c78f89762fcd208fde2
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-05 16:00:21 +0800

    测试观察者模式

---
+++ src/gof23/Observer2_ConcreteSubject.java
@@ -0,0 +1,25 @@
+package gof23;
+/**
+ * 通过继承Observable类，创建目标类
+ */
+import java.util.Observable;
+
+public class Observer2_ConcreteSubject extends Observable {
+	private int state;
+	
+	public void set(int s) {
+		this.state = s;
+		setChanged(); //表示目标对象已经发生改变
+		notifyObservers(state);//通知所有观察者
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	
+}

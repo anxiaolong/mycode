7b3841dbd9d89d8c58481e047fcbc09d2aafd16f
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-10 15:54:10 +0800

    使用正则表达式，实现爬取网站链接的爬虫

---
+++ src/regex/IgnoreCert.java
@@ -0,0 +1,58 @@
+package regex;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
+
+public class IgnoreCert {
+	public static void ingnoreCertificates(){
+		try {
+			HostnameVerifier hv = new HostnameVerifier() {
+				public boolean verify(String urlHostName, SSLSession session) {
+					System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
+					return true;
+				}
+			};
+			trustAllHttpsCertificates();
+			HttpsURLConnection.setDefaultHostnameVerifier(hv);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	private static void trustAllHttpsCertificates() throws Exception {
+		javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
+		javax.net.ssl.TrustManager tm = new miTM();
+		trustAllCerts[0] = tm;
+		javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, null);
+		javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	}
+
+	private static class miTM implements javax.net.ssl.TrustManager, javax.net.ssl.X509TrustManager {
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+			return null;
+		}
+
+		public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {
+			return true;
+		}
+
+		public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+			return true;
+		}
+
+		public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+				throws java.security.cert.CertificateException {
+			return;
+		}
+
+		public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+				throws java.security.cert.CertificateException {
+			return;
+		}
+	}
+}
---
+++ src/regex/MiguSpider.java
@@ -0,0 +1,133 @@
+package regex;
+/**
+ * 使用正则表达式，实现一个简单的爬虫
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+
+public class MiguSpider {
+	private static CopyOnWriteArrayList<String> miguUrl = new CopyOnWriteArrayList<String>();
+	static {
+		IgnoreCert.ingnoreCertificates();
+	}
+	public static String  getURLContent(String urlStr,String charset){
+		StringBuilder sb = new StringBuilder();
+		try {
+			URL url = new URL(urlStr);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(),Charset.forName(charset)));
+			String temp = "";
+			while((temp=reader.readLine())!=null){
+				sb.append(temp);
+			}
+		} catch (Exception e) {
+//			e.printStackTrace();
+			System.out.println("链接openStream()出错");
+		} 
+		return sb.toString();
+	}
+	
+	public static List<String> getMatherSubstrs(String destStr,String regexStr){
+		Pattern p = Pattern.compile(regexStr);
+		Matcher m = p.matcher(destStr);
+		List<String> result = new ArrayList<String>();
+		while(m.find()){
+			result.add(m.group(1));
+		}	
+		return result;
+	}
+	
+	public static int getUrl(String url) throws Exception {
+		Connection conn = Jsoup.connect(url)
+				.method(Method.GET).ignoreContentType(true);
+		Response rsp = conn.execute();
+		return rsp.statusCode();
+	}
+	
+	//向miguUrl中存入咪咕的链接
+	public static void urlTolist(String url) {
+		//设置主url地址
+		String destStr = getURLContent(url,"utf-8");
+		//获取源码里面的链接
+		List<String> result = getMatherSubstrs(destStr, "href=\"([\\w\\s./:]+?)\"");
+		
+		//链接补全，并存入新的list中
+		List<String> link = new ArrayList<String>();
+		for (String temp : result) {
+			if (temp.startsWith("//")) {
+				String newString = "http:"+temp;
+				link.add(newString);
+			} else if (temp.startsWith("/")) {
+				String newString = "http://music.migu.cn"+temp;
+				link.add(newString);
+			} else {
+				link.add(temp);
+			}
+		}
+		
+		//遍历link，存入miguUrl并去重
+		for (String string : link) {
+			if (!miguUrl.contains(string)) {
+				miguUrl.add(string);
+			}
+		}
+	}
+	
+	public static void nextLevel(int depth) {
+		for (int i = 0; i < depth; i++) {
+			for (String string : miguUrl) {
+				if (string.startsWith("http://music.migu.cn")||string.contains("i.migu.cn")) {
+					urlTolist(string);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		urlTolist("http://music.migu.cn/v3");
+		
+		nextLevel(1);
+		
+		FileOutputStream fos = new FileOutputStream(new File("c:/test/getResult"));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		PrintWriter pw = new PrintWriter(new File("c:/test/getError"));
+		int count = 0;
+		for (String string : miguUrl) {
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						bw.write("get结果-->"+getUrl(string)+"-->url-->"+string+"\r\n");
+						bw.flush();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						pw.write(string+"\r\n");
+						e.printStackTrace(pw);
+						pw.write("#######################分界#########################################"+"\r\n");
+						pw.flush();
+					} 
+				}
+			}).start();
+			count++;
+		}
+		System.out.println(count+"条数据");
+		
+	}
+}

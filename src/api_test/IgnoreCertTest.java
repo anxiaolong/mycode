5280af4d898595897d1ae82124a7661d4a734853
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-12 09:50:55 +0800

    提交网站巡检脚本

---
+++ src/api_test/IgnoreCertTest.java
@@ -0,0 +1,72 @@
+package api_test;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
+import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+
+public class IgnoreCertTest {
+	public static void ingnoreCertificates(){
+		try {
+			HostnameVerifier hv = new HostnameVerifier() {
+				public boolean verify(String urlHostName, SSLSession session) {
+					System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
+					return true;
+				}
+			};
+			trustAllHttpsCertificates();
+			HttpsURLConnection.setDefaultHostnameVerifier(hv);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+//	private static void main(String[] args) throws Exception {
+//		HostnameVerifier hv = new HostnameVerifier() {
+//			public boolean verify(String urlHostName, SSLSession session) {
+//				System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
+//				return true;
+//			}
+//		};
+//		trustAllHttpsCertificates();
+//		HttpsURLConnection.setDefaultHostnameVerifier(hv);
+//	}
+
+	private static void trustAllHttpsCertificates() throws Exception {
+		javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
+		javax.net.ssl.TrustManager tm = new miTM();
+		trustAllCerts[0] = tm;
+		javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, null);
+		javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	}
+
+	private static class miTM implements javax.net.ssl.TrustManager, javax.net.ssl.X509TrustManager {
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+			return null;
+		}
+
+		public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {
+			return true;
+		}
+
+		public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+			return true;
+		}
+
+		public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+				throws java.security.cert.CertificateException {
+			return;
+		}
+
+		public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+				throws java.security.cert.CertificateException {
+			return;
+		}
+	}
+}

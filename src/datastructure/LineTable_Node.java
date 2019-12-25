716548575416cb0e907ce4ff344f42ed5adf5897
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-25 11:28:14 +0800

    实现单链表

---
+++ src/datastructure/LineTable_Node.java
@@ -0,0 +1,40 @@
+package datastructure;
+/**
+ * 单链表的节点
+ * @author Administrator
+ *
+ */
+public class LineTable_Node {
+	Object data;//要存储的数据
+	LineTable_Node next;//下一个节点
+	
+	public LineTable_Node() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public LineTable_Node(Object data) {
+		this.data = data;
+	}
+	
+	public LineTable_Node(Object data,LineTable_Node next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public LineTable_Node getNext() {
+		return next;
+	}
+
+	public void setNext(LineTable_Node next) {
+		this.next = next;
+	}
+	
+}
---
+++ src/datastructure/LineTable_SingleLinkedList.java
@@ -0,0 +1,122 @@
+package datastructure;
+/**
+ * 实现一个单链表
+ * @author Administrator
+ *
+ */
+public class LineTable_SingleLinkedList implements LineTable_List {
+	//头节点，不存任何数据
+	private LineTable_Node head = new LineTable_Node();
+	//节点个数
+	private int size;
+	
+	
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public Object get(int i) {
+		//从头节点开始找
+		LineTable_Node p = head;
+		for (int j = 0; j <=i; j++) {
+			p = p.next;
+		}
+		return p.data;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size==0;
+	}
+
+	@Override
+	public boolean contains(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int indexOf(Object e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(int i, Object e) {
+		//判断i越界的问题
+		if (i<0||i>size) {
+			throw new LineTable_MyArrayIndexOutOfBoundsException("数组指针越界");
+		}
+		//找到i的前一个节点
+		LineTable_Node p = head;
+		for (int j = 0; j <i; j++) {
+			p = p.next;
+		}
+		//新建一个节点，存入e
+		LineTable_Node newNode = new LineTable_Node();
+		newNode.data =e;
+		//设置新节点的前驱和后继
+		newNode.next = p.next;//后继
+		p.next = newNode;//前驱
+		//add完了之后size++
+		size++;
+	}
+
+	@Override
+	public void add(Object e) {
+		this.add(size, e);
+	}
+
+	@Override
+	public void addBefore(Object obj, Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAfter(Object obj, Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object remove(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object replace(int i, Object e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		if (size==0) {
+			return "[]";
+		}
+		StringBuilder builder = new StringBuilder("[");
+		LineTable_Node p = head.next;
+		//遍历各个节点并取值
+		for (int i = 0; i < size; i++) {
+			if (i!=size-1) {
+				builder.append(p.data+",");
+			}else {
+				builder.append(p.data);
+			}
+			//移动节点
+			p=p.next;
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+}

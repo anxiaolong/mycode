3a59ddfd5a14429c877bc6239ead462e7d86c829
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-27 15:59:01 +0800

    测试sha1加密

---
+++ src/api_test/TestSha1.java
@@ -0,0 +1,12 @@
+package api_test;
+/**
+ * ≤‚ ‘sha1º”√‹
+ */
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class TestSha1 {
+	public static void main(String[] args) {
+		String sign = DigestUtils.sha1Hex("anxiaolong");
+		System.out.println(sign);
+	}
+}

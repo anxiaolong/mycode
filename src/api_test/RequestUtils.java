69d8e20dba5f49bd6d7771ab8c11f227db0906e4
Administrator <Administrator@DESKTOP-656J2TR>
2019-12-30 17:52:14 +0800

    接口测试update

---
+++ src/api_test/RequestUtils.java
@@ -0,0 +1,20 @@
+package api_test;
+
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+
+public class RequestUtils {
+	public static String postData(String url,String body,String header) throws IOException {
+		Connection conn = Jsoup.connect(url)
+				.method(Method.POST).ignoreContentType(true)
+				.header("Authorization", header)
+				.header("Content-Type", "application/json")
+				.requestBody(body);
+		Response rsp = conn.execute();
+		return "²ÎÊý£º"+body+" -> "+rsp.statusCode() + " -> " + rsp.body();
+	}
+}

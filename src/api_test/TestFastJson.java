45937a3af6d95a6f19f7925a4e998aa13058298e
Administrator <Administrator@DESKTOP-656J2TR>
2020-01-02 10:11:06 +0800

    测试fastjson创建json对象和解析json对象

---
+++ src/api_test/TestFastJson.java
@@ -0,0 +1,43 @@
+package api_test;
+/**
+ * 测试fastjson创建json对象和解析json对象
+ */
+import java.util.Arrays;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class TestFastJson {
+	//创建json对象
+	public static JSONObject creatJsonObject() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", "anxiaolong");
+		jsonObject.put("age", 27);
+		jsonObject.put("job", "test");
+		jsonObject.put("sex", 1);
+		List<String> list = Arrays.asList("dfdf","fdfd","oooo","fdffff");
+		jsonObject.put("infos", list);
+		jsonObject.put("null", "null");
+		jsonObject.put("boolean", true);
+//		System.out.println(jsonObject);
+		return jsonObject;
+	}
+	
+	//解析json对象
+	public static void parseJsonObject() {
+		System.out.println(creatJsonObject().toString());
+		String jsonString = creatJsonObject().toString();
+		JSONObject jsonObject = JSONObject.parseObject(jsonString);
+		System.out.println(jsonObject.get("boolean"));
+		System.out.println(jsonObject.get("null"));
+		System.out.println(jsonObject.get("sex"));
+		System.out.println(jsonObject.get("name"));
+		System.out.println(jsonObject.get("job"));
+		System.out.println(jsonObject.get("age"));
+		System.out.println(jsonObject.get("infos"));
+	}
+	
+	public static void main(String[] args) {
+		parseJsonObject();
+	}
+}	

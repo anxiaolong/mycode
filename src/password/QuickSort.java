abbffc2be4de0dfab45e1389535d647f4aa5f928
Administrator <Administrator@DESKTOP-656J2TR>
2020-01-09 16:55:30 +0800

    update

---
+++ src/password/QuickSort.java
@@ -0,0 +1,16 @@
+package password;
+
+import java.util.Arrays;
+
+public class QuickSort {
+	public static void main(String[] args) {
+		int[] arr = {89,45,32,56,77,88,43,12,44,51};
+		System.out.println(Arrays.toString(arr));
+		quickSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void quickSort(int[] arr) {
+		
+	}
+}

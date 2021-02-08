143dfca97d457b533bc27bb29fa9ee3abd6da7cd
anxiaolong <anxiaolong@mypc>
2021-02-08 20:33:40 +0800

    upd fastjson

--- src/java_apply01_api_test/TestFastJson.java
+++ src/java_apply01_api_test/TestFastJson.java
@@ -1,47 +1,78 @@
 package java_apply01_api_test;
 /**
- * 测试fastjson创建json对象和解析json对象
+ * 测试fastjson常用方法
  */
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 public class TestFastJson {
-	//创建json对象
-	public static JSONObject creatJsonObject() {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("name", "anxiaolong");
-		jsonObject.put("age", 27);
-		jsonObject.put("job", "test");
-		jsonObject.put("sex", 1);
-		List<String> list = Arrays.asList("dfdf","fdfd","oooo","fdffff");
-		jsonObject.put("infos", list);
-		jsonObject.put("null", "null");
-		jsonObject.put("boolean", true);
-//		System.out.println(jsonObject);
-		return jsonObject;
+	public int id;
+	public String name;
+	public String pwd;
+	public static TestFastJson tfj=new TestFastJson(11, "anxiaolong", "123456");
+	
+	@Override
+	public String toString() {
+		return "TestFastJson [id=" + id + ", name=" + name + ", pwd=" + pwd + "]";
 	}
 	
-	//解析json对象
-	public static void parseJsonObject() {
-		System.out.println(creatJsonObject().toString());
-		String jsonString = creatJsonObject().toString();
-		JSONObject jsonObject = JSONObject.parseObject(jsonString);
-		System.out.println(jsonObject.get("boolean"));
-		System.out.println(jsonObject.get("null"));
-		System.out.println(jsonObject.get("sex"));
-		System.out.println(jsonObject.get("name"));
-		System.out.println(jsonObject.get("job"));
-		System.out.println(jsonObject.get("age"));
-		System.out.println(jsonObject.get("infos"));
-		List<String> list = (List<String>) jsonObject.get("infos");
-		for (String string : list) {
-			System.out.println(string);
-		}
+	public TestFastJson(int id,String name,String pwd) {
+		this.id=id;
+		this.name=name;
+		this.pwd=pwd;
+	}
+	
+	//1.Java对象转json字符串
+	public static void pojo2JsonString() {
+		System.out.println(JSON.toJSONString(tfj));
+	}
+	//2.Java对象转Json对象
+	public static void pojo2JsonObj() {
+		System.out.println(JSONObject.toJSON(tfj));
+	}
+	//3.json对象转json字符串
+	public static void jsonObj2JsonStr() {
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("id", "11");
+		jsonObject.put("name", "tetttt");
+		jsonObject.put("pwd", "111111");
+		System.out.println(jsonObject.toJSONString());
+	}
+	//4.json对象转java对象
+	public static void jsonObj2JavaObj() {
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("id", 11);
+		jsonObject.put("name", "tetttt");
+		jsonObject.put("pwd", "111111");
+		System.out.println(JSON.toJavaObject(jsonObject, TestFastJson.class));
+	}
+	//5.json字符串装json对象
+	public static void jsonStr2JsonObj() {
+		String string= "{\"id\":11,\"name\":\"andd\",\"pwd\":\"111111\"}";
+		JSONObject jsonObject=JSON.parseObject(string);
+		System.out.println(jsonObject);
+	}
+	//6.json字符串转java对象
+	public static void jsonStr2JavaObj() {
+		String string= "{\"id\":11,\"name\":\"andd\",\"pwd\":\"111111\"}";
+		TestFastJson ttt = JSON.parseObject(string, TestFastJson.class);
+		System.out.println(ttt);
 	}
 	
 	public static void main(String[] args) {
-		parseJsonObject();
+		pojo2JsonString();
+		pojo2JsonObj();
+		jsonObj2JsonStr();
+		jsonObj2JavaObj();
+		jsonStr2JsonObj();
+		jsonStr2JavaObj();
+		List<TestFastJson> list=new ArrayList<TestFastJson>();
+		list.add(new TestFastJson(1, "dddd", "111111"));
+		list.add(new TestFastJson(2, "anxiaolong", "333333"));
+		list.add(new TestFastJson(3, "liudeeee", "44455664"));
+		System.out.println(JSON.toJSONString(list));
 	}
 }	

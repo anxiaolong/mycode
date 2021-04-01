5bec7adacdc664f908fda403a59550125060431f
anxiaolong <anxiaolong@mypc>
2021-04-01 17:39:50 +0800

    update

--- src/java_01_grammar/TestConstant.java
+++ src/java_01_grammar/TestConstant.java
@@ -1,5 +1,4 @@
 package java_01_grammar;
-import org.omg.PortableInterceptor.AdapterManagerIdHelper;
 
 /**
  * 常量用final修饰，被初始化之后不能再修改值
@@ -9,7 +8,7 @@ import org.omg.PortableInterceptor.AdapterManagerIdHelper;
 public class TestConstant {
 	public static void main(String[] args) {
 		final int age = 18;
-//		age =20; 修改常量值会异常
-		
+		//age =20; //修改常量值会异常
+		System.out.println(age+"--age是final修饰的变量不可修改");
 	}
 }
